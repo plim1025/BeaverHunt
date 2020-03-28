c6ec7b144e92f9a370fde7c0b506e2fb3bb43333
plim1025 <plim1025@gmail.com>
2020-03-28 14:23:39 -0700

    Added game board

--- Beaver Hunt/Assets/Scripts/Beaver.cs
+++ Beaver Hunt/Assets/Scripts/Beaver.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Beaver : MonoBehaviour
 {
+    void Start()
+    {
 
+    }
+    private void Update()
+    {
+        // We add +1 to the x axis every frame.
+        // Time.deltaTime is the time it took to complete the last frame
+        // The result of this is that the object moves one unit on the x axis every second
+        transform.position += new Vector3(1 * Time.deltaTime, 0, 0);
+    }
     
 }
