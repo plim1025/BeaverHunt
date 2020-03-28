0dfca3ed4f135cd95614a02cb062b467cd6e42bc
YiuTran <yiutran@gmail.com>
2020-03-28 15:36:41 -0700

    Player Movement + Layers

--- Beaver Hunt/Assets/Scripts/Duck.cs
+++ Beaver Hunt/Assets/Scripts/Duck.cs
@@ -4,6 +4,31 @@ using UnityEngine;
 
 public class Duck : MonoBehaviour
 {
+/*     public float speed = 3.9f;
+
+    public Node startarea;
+    public int scattermode1 = 7;
+    public int chasemode1 = 7;
+    public int scattermode2 = 7;
+    public int chasemode2 = 20;
+    public int scattermode3 = 7;
+    public int chasemode3 = 20;
+    public int scattermode3 = 5;
+    public int chasemode3 = 20;
+    public int scattermode4 = 6;
+    public int chasemode4 = 20;
+
+
+
+    private int modechange = 1;
+    private float modetimer = 0;
+
+    public enum Mode{
+        Chase, Scatter, Frightened
+    }
+
+    Mode current = Mode.Scatter;
+    Mode previousMode; */
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +39,9 @@ public class Duck : MonoBehaviour
     void Update()
     {
         
+        // We add +1 to the x axis every frame.
+        // Time.deltaTime is the time it took to complete the last frame
+        // The result of this is that the object moves one unit on the x axis every second
+        transform.position += new Vector3(1 * Time.deltaTime, 0, 0);
     }
 }
