7ac28d38459d34cc347ebea0c335c49edb480cc9
plim1025 <plim1025@gmail.com>
2020-03-28 20:12:24 -0700

    Spawned wood

---
+++ Beaver Hunt/Assets/Scripts/SpawnPlanks.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlanks : MonoBehaviour
+{
+    public GameObject plankPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPlanks();
+    }
+
+    private void spawnPlanks() {
+        for(int i = -3; i <= 3; i++) {
+            for(int j = -3; j <= 3; j++) {
+                GameObject plank = Instantiate(plankPrefab) as GameObject;
+                plank.transform.position = new Vector3(i*10, j*10, 0);        
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
